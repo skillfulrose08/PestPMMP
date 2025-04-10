655f4b4f8c1b90639e4085b74927ade7b28a43f2
Elysio Martins <elysiomartinspro@gmail.com>
2025-04-10 23:24:13 +0200

    feat: add fake player creation

--- src/tests/PocketMineTester.php
+++ src/tests/PocketMineTester.php
@@ -2,10 +2,12 @@
 
 namespace Refaltor\PestPmmpTests\tests;
 
-use muqsit\fakeplayer\FakePlayer;
+use muqsit\fakeplayer\info\FakePlayerInfoBuilder;
 use muqsit\fakeplayer\Loader;
 use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginBase;
+use pocketmine\promise\Promise;
+use Ramsey\Uuid\Uuid;
 
 class PocketMineTester
 {
@@ -15,11 +17,16 @@ class PocketMineTester
     {
         self::setPlugin($plugin);
 
-        $pluginsLoaded = $plugin->getServer()->getPluginManager()->loadPlugins(__DIR__ . "../../resources/fakeplayer.phar");
+        $pluginsLoaded = $plugin->getServer()->getPluginManager()->loadPlugins(__DIR__ . "/../../resources/fakeplayer.phar");
         if (count($pluginsLoaded) <= 0) {
             $plugin->getLogger()->error("Fake Player plugin not loaded. Please check PestPMMP installation.");
             $plugin->getServer()->shutdown();
         }
+    }
+
+    private static function getFakePlayerPlugin(): Plugin|Loader
+    {
+        return self::getPlugin()->getServer()->getPluginManager()->getPlugin("FakePlayer");
     }
 
     public static function getPlugin(): PluginBase
@@ -32,8 +39,14 @@ class PocketMineTester
         self::$plugin = $plugin;
     }
 
-    public static function createFakePlayer(): void
+    public static function createFakePlayer(string $name): Promise
     {
+        $fakePlayerPlugin = self::getFakePlayerPlugin();
 
+        return $fakePlayerPlugin->addPlayer(FakePlayerInfoBuilder::create()
+            ->setUsername($name)
+            ->setXuid(Uuid::uuid1()->toString())
+            ->setUuid(Uuid::uuid4())
+            ->build());
     }
 }
